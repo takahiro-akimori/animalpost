9a1dba1f59eb69c7cde5ddde7eeca945e56cdef9
takahiro.akimori <tak0316.a@gmail.com>
2019-07-09 07:35:46 +0000

    user registration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,18 +6,15 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+    
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('password',60);
+            $table->string('animals');
             $table->rememberToken();
             $table->timestamps();
         });
